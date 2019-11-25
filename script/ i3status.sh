885937fe6fcadec17455cd5202e817387ed58a54
SVDevelop <verstka.sv@mail.ru>
2019-11-25 16:47:29 +0300

    123

---
+++ script/ i3status.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# shell script to prepend i3status with more stuff
+ 
+i3status | while :
+do
+    read line
+    layouts=$(setxkbmap -query | awk '/layout/{print $2}')
+    current=$(dbus-send --print-reply=literal --dest=ru.gentoo.KbddService /ru/gentoo/KbddService ru.gentoo.kbdd.getCurrentLayout | awk '{ print $2 }')
+    index=$(($current+1))
+    lang=$(echo $layouts | cut -d, -f$index)
+    wifi=$(nmcli d wifi | grep "^\*" | grep -v "\*.*SSID" | awk '{ print $8 }')
+    datetime=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "$lang | W: $wifi | $line | $datetime" || exit 1
+done
